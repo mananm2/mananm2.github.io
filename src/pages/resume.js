f6fb925f314cc742a181e16519bb7808e823611f
Manan Mehta <mananmehta@Manans-Air.attlocal.net>
2023-05-19 14:06:47 -0500

    test

--- src/pages/resume.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-import PDFViewer from "../components/PDFViewer"
-
-export default () => (
-  <Layout>
-    <PDFViewer file={"/avinash_madavan_resume.pdf"}/>
-  </Layout>
-)
